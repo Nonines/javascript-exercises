0749daf58fdb0ffc6b533991e811bd87260411e4
Banky <kingmorty@protonmail.com>
2022-10-28 22:45:46 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = [];
 
+    for (let letter of string) {
+        stringArray.push(letter);
+    }
+
+    let reversedArray = stringArray.reverse();
+    let reversedString = "";
+
+    for (let item of reversedArray) {
+        reversedString += item;
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
