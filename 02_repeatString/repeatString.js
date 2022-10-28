e65ca44888a7fcdcc7b6394a5b0e82ef82b19cb1
Banky <kingmorty@protonmail.com>
2022-10-28 22:15:53 +0100

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    let repeatedString = "";
 
+    if (count < 0) {
+        return "ERROR";
+    }
+
+    let i = 0;
+    while (i < count) {
+        repeatedString += string;
+        i++;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
