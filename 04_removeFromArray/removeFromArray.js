7cbb602a20e1e15434783ead6db74d4b63c26e2e
Banky <kingmorty@protonmail.com>
2022-10-29 00:40:07 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let arg of args) {
+        const indexToRemove = arr.indexOf(arg);
+        if (indexToRemove > -1) {
+            arr.splice(indexToRemove, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
