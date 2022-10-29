ac59da9da26a6b687cb8c3a1af2fa27c1fbd0f6f
Banky <kingmorty@protonmail.com>
2022-10-29 01:15:38 +0100

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
 
+    if (typeof numA !== "number" || typeof numB !== "number") {
+        return "ERROR";
+    } else if (numA < 0 || numB < 0) {
+        return "ERROR";
+    }
+
+    const numArray = [];
+    numArray.push(numA, numB);
+    numArray.sort(function(a, b){return a - b});
+
+    let start = numArray[0];
+    let end = numArray[1];
+    let total = 0;
+
+    for (i = start; i <= end; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
