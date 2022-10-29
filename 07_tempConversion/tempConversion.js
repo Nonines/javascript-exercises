4cc021ac0cbf93ef63bfbe81eb71c470896e3bf4
Banky <kingmorty@protonmail.com>
2022-10-29 11:51:54 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  // take in a fahr. value and return it's equivalent in celsius
+  // ((f - 32) * 5) / 9  math formula
 
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  const roundedCelsius = +celsius.toFixed(1);
+  return roundedCelsius;
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  // take in a cels. value and return it's equivalent in fahrenheit
+  // ((c * 9) / 5) + 32  formula
 
+  const fahrenheit = ((celsius * 9) / 5) + 32;
+  const roundedFahrenheit = +fahrenheit.toFixed(1);
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
