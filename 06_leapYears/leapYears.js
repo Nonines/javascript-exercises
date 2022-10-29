c1590f35fdd8049ad72fda09c9399e374135e09f
Banky <kingmorty@protonmail.com>
2022-10-29 11:15:13 +0100

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true;  // is leap year
 
+    } else if (year % 100 === 0) {
+        return false;  // is not leap year
+
+    } else if (year % 4 === 0) {
+        return true; // is leap year
+    } else {
+        return false; // is not leap year
+    }
 };
 
 // Do not edit below this line
