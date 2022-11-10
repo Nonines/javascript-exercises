fbfecbc2bca3afd55203c82e6cebb6547e1e87c9
Banky <kingmorty@protonmail.com>
2022-11-10 19:45:09 +0100

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  for (let num of arr) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let total = 1;
+  for (let arg of args) {
+    total *= arg;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  fact = 1;
+  for (i=num; i>0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
