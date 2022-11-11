d00f7948bd04e50cf85b5fbefe6641275dbdee4d
Banky <kingmorty@protonmail.com>
2022-11-11 01:09:19 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const cleanInput = input.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    let inputStringArr = [];
 
+    for (let char of cleanInput) {
+        inputStringArr.push(char);
+    }
+
+    const reverseString = inputStringArr.reverse().join("");
+
+    if (cleanInput === reverseString) {
+        return true;
+    }
+    return false;
 };
+
+(palindromes("A car, a man, a maraca."))
 
 // Do not edit below this line
 module.exports = palindromes;
