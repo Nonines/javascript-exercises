a00bebf3d9dc60b565057d8403e7d02cb4714c27
Banky <kingmorty@protonmail.com>
2022-11-12 09:54:45 +0100

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookObjArray) {
+    return bookObjArray.map(function (obj) {
+        return obj.title;
+    })
 };
 
 // Do not edit below this line
