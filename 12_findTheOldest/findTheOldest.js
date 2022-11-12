0904c4f764e3e27848313677efb55458017a6947
Banky <kingmorty@protonmail.com>
2022-11-12 11:15:25 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleObjArr) {
+    const date = new Date();
+    const arr = peopleObjArr.sort(function (a, b) {
 
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = date.getFullYear();
+        } else if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = date.getFullYear();
+        }
+
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    });
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
